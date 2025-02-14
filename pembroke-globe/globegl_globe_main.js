189024b1954ec1e38ba738bf1b80df9be2e7e076
psjm3 <psjm3@cam.ac.uk>
2025-02-14 11:17:20 +0000

    Publish a sample globe to github pages

---
+++ pembroke-globe/globegl_globe_main.js
@@ -0,0 +1,161 @@
+import './globe.css'
+import Globe from "./globegl.js";
+
+const myGlobe = Globe();
+
+// Title Pembroke Women: A World of Impact
+
+myGlobe(document.getElementById('app'))
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-blue-marble.jpg')
+    .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
+    .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png');
+
+myGlobe.controls().autoRotate = true;
+myGlobe.controls().autoRotateSpeed = 0.8;
+
+var camToSave = {};
+camToSave.position = myGlobe.camera().position.clone();
+camToSave.rotation = myGlobe.camera().rotation.clone();
+
+function showProfiles() {
+    stopDistribution();
+    myGlobe.camera().position.set(camToSave.position.x, camToSave.position.y, camToSave.position.z);
+    myGlobe.controls().autoRotate = false;
+
+    //fetch('./real-data/pembroke_selected_alumni.arr')
+    fetch('./sample-data/samples.arr')
+    .then(response => response.json()
+    .then(json => {
+            console.log(json);
+            myGlobe
+                .htmlElementsData(json)
+                .htmlElement(d => {
+                    const el = document.createElement('div');
+                    el.setAttribute('id', "thumbnail-div");
+                    let img = document.createElement('img');
+                    img.setAttribute('class', "thumbnail-img");
+                    img.setAttribute('id', "thumbnail-img");
+                    img.setAttribute('src', d.imageUrl);
+                    img.setAttribute('width', 30);
+                    img.setAttribute('height', 30);
+                    el.appendChild(img);
+                    el.style['pointer-events'] = 'auto';
+                    el.style.cursor = 'pointer';
+                     el.onclick= (e) => setToFocus(e, d);
+                    return el;
+                })
+        })
+    );
+    // fetch('../data/pembroke_alumni.geojson')
+    //     .then(res => res.json())
+    //     .then(countries => {
+    //         myGlobe
+    //             .labelsData(countries.features)
+    //             .labelLat(d => d.properties.latitude)
+    //             .labelLng(d => d.properties.longitude)
+    //             //.labelText(d => d.properties.shortname)
+    //             .labelColor(() => 'rgb(255, 255, 0)')
+    //             .labelSize(1)
+    //             .labelResolution(10)
+    //             .labelDotRadius(0);
+    //         setTimeout(() => myGlobe
+    //             .labelsTransitionDuration(4000)
+    //             .labelAltitude(
+    //                 d =>
+    //                     Math.max(0.05, Math.sqrt(+d.properties.labelrank)*0.05))
+    //             // .htmlTransitionDuration(4000)
+    //             // .htmlAltitude(d => Math.max(0.05, Math.sqrt(+d.altitude)*0.05))
+    //         , 2000);
+    //     });
+}
+
+function setToFocus(e, data) {
+    stopProfiles();
+    myGlobe
+        .pointOfView({ lat: data.lat, lng: data.lng, altitude: data.altitude }, 0)
+        .htmlElementsData([{
+            "name":data.name,
+            "imageUrl":data.imageUrl,
+            "lat":data.lat,
+            "lng":data.lng,
+            "altitude":data.altitude,
+            "matriculation_year":data.matriculation_year,
+            "city_region":data.city_region,
+            "country": data.country,
+            "job": data.job,
+            "organisation": data.organisation
+        }])
+        .htmlElement(thisData => {
+            let popup = document.createElement('div');
+            popup.setAttribute('class', 'card');
+            let img = document.createElement('img');
+            img.setAttribute('src', thisData.imageUrl);
+            img.setAttribute('alt', thisData.name);
+            img.setAttribute('width', "20%");
+            img.setAttribute('height', "20%");
+            let container = document.createElement('div');
+            container.setAttribute('class', 'container');
+            container.innerHTML = `
+                <h2>${thisData.name}</h2>
+                <p class="profileText">Matriculation Year: ${thisData.matriculation_year}
+                <br>Country: ${thisData.country}
+                <br>Job: ${thisData.job}
+                <br>Organisation: ${thisData.organisation}</p>
+                `;
+            popup.appendChild(img);
+            popup.appendChild(container);
+            return popup;
+        });
+}
+
+function stopProfiles() {
+    myGlobe.labelsData([]);
+}
+
+function showDistribution() {
+    stopProfiles();
+    myGlobe.controls().autoRotate = true;
+
+    fetch('./data/All_pembroke_women.arr')
+    .then(response => 
+        response.json().then(json => {
+            myGlobe.htmlElementsData(json)})
+    );
+    myGlobe.htmlElement(d => {
+            const el = document.createElement('div');
+            el.innerHTML = `<svg viewBox="-4, 0, 36 36"><path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path><circle fill="black" cx="14" cy="14" r="7"></circle></svg>`;
+            el.style.color = d.color;
+            el.style.width = `${d.size}px`;
+            el.style['pointer-events'] = 'auto';
+            el.style.cursor = 'pointer';
+            return el;
+        });
+}
+
+function stopDistribution() {
+    myGlobe.htmlElementsData([]);
+}
+
+function toggleRotation() {
+    myGlobe.controls().autoRotate = !myGlobe.controls().autoRotate;
+}
+
+const viewProfiles = document.getElementById("viewProfiles");
+viewProfiles.addEventListener('click', showProfiles);
+
+const viewDist = document.getElementById("viewDist");
+viewDist.addEventListener('click', showDistribution);
+
+function toggleView() {
+    console.log(viewButton.value);
+
+    if (viewButton.value === 'View - Highlights') {
+        stopDistribution();
+        showHighlights();
+        viewButton.value = 'View - Distribution';
+    } else {
+        stopProfiles();
+        showDistribution();
+        viewButton.value = 'View - Highlights';
+    }
+}
---
+++ pembroke-globe/sample-data/samples.arr
@@ -0,0 +1,51 @@
+[
+    {
+    
+      "name": "Black Widow",
+      "lat":48.8047222,
+      "lng":44.5858345,
+      "altitude":1,
+      "imageUrl": "./sample-data/black-widow.jpg",
+      "matriculation_year": 2001,
+      "city_region": "",
+      "country": "USA",
+      "job": "A highly skilled spy, assassin, and intelligence operative",
+      "organisation": "Marvel Universe"
+    },
+    {
+      "name": "Gamora",
+      "lat":35.0844909,
+      "lng":-106.6511383,
+      "altitude":1,
+      "imageUrl": "./sample-data/Gamora.avif",
+      "matriculation_year": 1990,
+      "city_region": "",
+      "country": "Planet Zen-Whoberi",
+      "job": "A skilled warrior and assassin",
+      "organisation": "Marvel Universe"
+    },
+    {
+      "name": "Captain Marvel",
+      "lat":38.8951118,
+      "lng":-77.0363693,
+      "altitude":1,
+      "imageUrl": "./sample-data/captain_marvel.webp",
+      "matriculation_year": 2014,
+      "city_region": "",
+      "country": "United Kingdom",
+      "job": "A powerful superhero, former Air Force pilot, and intergalactic warrior",
+      "organisation": "Marvel Universe"
+    },
+    {
+      "name": "Scarlet Witch",
+      "lat":46.0552778,
+      "lng":14.5144444,
+      "altitude":1,
+      "imageUrl": "./sample-data/scalet_witch.avif",
+      "matriculation_year": 2020,
+      "city_region": "",
+      "country": "United Kingdom",
+      "job": "A powerful sorceress and mutant",
+      "organisation": "Marvel Universe"
+    }
+  ]
